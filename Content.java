52bc749e3ed3546374626d7125f712234846ff79
ricky <rickyputra98@gmail.com>
2018-04-21 00:42:13 +0700

    add file picker

--- Content.java
+++ Content.java
@@ -1,7 +1,8 @@
 import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 import javax.swing.*;
 
-public class Content extends JPanel {
+public class Content extends JPanel implements ActionListener {
 
 	private JLabel plaintext, key, ciphertext;
     private JTextField sourceField, keyField, targetFileName;
@@ -65,6 +66,21 @@ public class Content extends JPanel {
         targetFileName.setLocation(250, 170);
         targetFileName.setSize(220, 20);
 
+        openFile = new JButton("Source");
+        openFile.setSize(150, 20);
+        openFile.setLocation(550, 70);
+        openFile.addActionListener(this);
+
+        openKey = new JButton("Key");
+        openKey.setSize(150, 20);
+        openKey.setLocation(550, 120);
+        openKey.addActionListener(this);
+
+        saveFile = new JButton("Target");
+        saveFile.setSize(150, 20);
+        saveFile.setLocation(550, 170);
+        saveFile.addActionListener(this);
+
         decrypt = new JButton("Decrypt");
         decrypt.setSize(100, 20);
         decrypt.setLocation(230, 230);
@@ -74,21 +90,24 @@ public class Content extends JPanel {
         encrypt.setSize(100, 20);
         encrypt.setLocation(400, 230);
         // encrypt.addActionListener(this);
+    }
 
-        openFile = new JButton("Source");
-        openFile.setSize(150, 20);
-        openFile.setLocation(550, 70);
-        // openFile.addActionListener(this);
-
-        openKey = new JButton("Key");
-        openKey.setSize(150, 20);
-        openKey.setLocation(550, 120);
-        // openKey.addActionListener(this);
-
-        saveFile = new JButton("Target");
-        saveFile.setSize(150, 20);
-        saveFile.setLocation(550, 170);
-        // saveFile.addActionListener(this);
+    @Override
+    public void actionPerformed(ActionEvent aksi) {
+        if (aksi.getSource() == openFile) {
+            FilePicker fpSource = new FilePicker();
+            sourceField.setText(fpSource.path);
+        } else if (aksi.getSource() == openKey) {
+            FilePicker fpKey = new FilePicker();
+            keyField.setText(fpKey.path);
+        } else if (aksi.getSource() == saveFile) {
+            FilePicker fpTarget = new FilePicker();
+            targetFileName.setText(fpTarget.path);
+        } else if (aksi.getSource() == encrypt) {
+            // TODO
+        } else if (aksi.getSource() == decrypt) {
+            // TODO
+        }
     }
 
 }
